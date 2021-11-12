e3b1d125a3c67128126e81a0cbbff50cacf334a1
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-11-12 22:57:03 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,9 +1,11 @@
 function calc() {
   let x1 = prompt("input x1");
-  let x2 = prompt("input x2");
   let sign = prompt("input sign");
+  let x2 = prompt("input x2");
+  document.write(x1 + sign + x2 + "=");
   let f;
-  let functionStr = "f = function(x){return x1" + sign + "x2;}";
+  let functionStr = "f = function(x){return " + x1 + sign + x2 + "}";
   eval(functionStr);
-  alert(f());
+  document.write(f());
 }
+
